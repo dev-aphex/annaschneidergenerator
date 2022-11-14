a7a5d87dfb9bcd52080107c8f78d38ebd2385c59
dev-aphex <dev-aphex@users.noreply.github.com>
2022-11-14 14:39:36 +0100

    optimierung

--- anna.py
+++ anna.py
@@ -1,14 +1,12 @@
 import random
-promi = ('NameA', 'NameB', 'NameC', 'NameD', 'NameE',)
-rnd_promi = random.randint(0, len(promi)-1)
-
+promi = ('Dieter Nuhr', 'Ulf Poschardt', 'Friedrich Merz', 'Wolfgang Kubicki', 'Anna Schneider',)
 buzzword = ('woke', 'linke', 'gendernde', 'grüne', 'autohassende',)
-rnd_buzzword = random.randint(0, len(buzzword)-1)
-
 buzzword2 = ('die Demokratie', 'unsere Gesellschaft', 'Deutschland', 'alle Liberale', 'die Meinungsfreiheit',)
-rnd_buzzword2 = random.randint(0, len(buzzword2)-1)
 
-print(promi[rnd_promi] + " ist also ein Nazi. Das behauptet zumindest die " +
-      buzzword[rnd_buzzword] + " Bubble auf Twitter. Mein Gefühl sagt mir, das ist eine Gefahr für " +
-      buzzword2[rnd_buzzword2] + ".")
-print('Ihre Anna Schneider.')
+for x in range(10):
+    rnd_promi = random.randint(0, len(promi) - 1)
+    rnd_buzzword = random.randint(0, len(buzzword) - 1)
+    rnd_buzzword2 = random.randint(0, len(buzzword2) - 1)
+    print(promi[rnd_promi] + " ist also ein Nazi. Das behauptet zumindest die " +
+          buzzword[rnd_buzzword] + " Bubble auf Twitter. Mein Gefühl sagt mir, das ist eine Gefahr für " +
+          buzzword2[rnd_buzzword2] + ".")
